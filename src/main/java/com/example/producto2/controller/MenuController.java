b5384946db49708d61f428361ef026777f3f95d1
alu10213035 <martaortega2411@gmail.com>
2023-01-30 17:11:40 +0100

    4

--- src/main/java/com/example/producto2/controller/MenuController.java
+++ src/main/java/com/example/producto2/controller/MenuController.java
@@ -1,11 +1,13 @@
 package com.example.producto2.controller;
 
+import com.example.producto2.model.Menu;
+import com.example.producto2.model.Producto;
 import com.example.producto2.service.menuService.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
@@ -15,10 +17,43 @@ public class MenuController {
     private MenuService menuService;
 
     @GetMapping("/menus")
-    public String home(Model model){
-        model.addAttribute("menus",menuService.findAll());
+    public String home(Model model) {
+        model.addAttribute("menus", menuService.findAll());
 
         return "menu";
     }
+
+    @GetMapping(value = "/mostrar")
+    public String mostrarMenu(Model model) {
+        model.addAttribute("menus", menuService.findAll());
+        return "menu";
+    }
+
+    @GetMapping("/menus/del/{id}")
+    public String eliminarMenu(@PathVariable("id") long id, RedirectAttributes redirectAttrs) {
+        menuService.delete(id);
+        redirectAttrs
+                .addFlashAttribute("mensaje", "Agregado correctamente")
+                .addFlashAttribute("clase", "success");
+        return "redirect:/mostrar";
+    }
+
+    @PostMapping(value = "/edit")
+    public String actualizarMenu(@ModelAttribute Menu menu, RedirectAttributes redirectAttrs) {
+        menuService.update(menu);
+        redirectAttrs
+                .addFlashAttribute("mensaje", "Editado correctamente")
+                .addFlashAttribute("clase", "success");
+        return "redirect:/mostrar";
+    }
+
+    @GetMapping(value="/edit/{id}")
+    public String editarMenu(@PathVariable Long id, Model model) {
+        model.addAttribute("menus", menuService.findById(id));
+        return "edit_menu";
+    }
+
+
+
 }
 
